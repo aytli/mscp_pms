11b23b34df075eafda191ae02b2b857c4ef1379c
Andy Li <aytli@live.com>
2016-07-09 15:48:09 -0400

    developed state to handle can bus commands/packets

--- main.c
+++ main.c
@@ -4,19 +4,53 @@
 
 #define HEARTBEAT_PERIOD_MS 200
 
-static int1          gb_send;
-static int32         g_can0_id;
-static int8          g_can0_data[8];
-static int8          g_can0_len;
-static int1          gb_can0_hit = false;
-static int32         g_can1_id;
-static int8          g_can1_data[8];
-static int8          g_can1_len;
-static int1          gb_can1_hit = false;
-static int32         g_rx_id;
-static int8          g_rx_len;
-static int8          g_rx_data[8];
-static pms_state_t   g_state;
+#define BPS_TEMP_WARNING  60 // 60°C charge limit
+#define BPS_TEMP_CRITICAL 70 // 70°C discharge limit
+
+// CAN bus defines
+#define TX_PRI 3
+#define TX_EXT 0
+#define TX_RTR 0
+
+#define ARRAY_ON                \
+    gb_array_connected = true;  \
+    output_high(MPPT_PIN);
+
+#define ARRAY_OFF               \
+    gb_array_connected = false; \
+    output_low(MPPT_PIN);
+
+static int1        gb_send;
+static int32       g_can0_id;
+static int8        g_can0_data[8];
+static int8        g_can0_len;
+static int1        gb_can0_hit = false;
+static int32       g_can1_id;
+static int8        g_can1_data[8];
+static int8        g_can1_len;
+static int1        gb_can1_hit = false;
+static int32       g_rx_id;
+static int8        g_rx_len;
+static int8        g_rx_data[8];
+static pms_state_t g_state;
+
+static int1        gb_array_connected;
+
+// Accepts a packet of BPS temperature data and the length of the packet
+// Checks if each point of data is below the temperature warning threshold
+int1 check_bps_temperature(int * data, int length)
+{
+    int i;
+    
+    for (i = 0 ; i < length ; i++)
+    {
+        if (data[i] >= BPS_TEMP_WARNING)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 // INT_TIMER2 programmed to trigger every 1ms with a 20MHz clock
 // This interrupt will toggle the heartbeat LED
@@ -105,9 +139,31 @@ void data_received_state(void)
 {
     switch(g_rx_id)
     {
+        case COMMAND_PMS_DISCONNECT_ARRAY_ID:
+            ARRAY_OFF;
+            can_putd(COMMAND_PMS_DISCONNECT_ARRAY_ID,0,0,TX_PRI,TX_EXT,TX_RTR);
+            break;
+        case CAN_BPS_TEMPERATURE1_ID:
+        case CAN_BPS_TEMPERATURE2_ID:
+        case CAN_BPS_TEMPERATURE3_ID:
+            if (check_bps_temperature(g_rx_data,g_rx_len) == 0)
+            {
+                // One of the battery temperatures is above the warning threshold
+                // Turn off the array
+                ARRAY_OFF;
+            }
+            else if (gb_array_connected == false)
+            {
+                // The battery temperatures are all below the warning threshold
+                // Turn on the array if it is off
+                ARRAY_ON;
+            }
+            break;
         default:
             break;
     }
+    
+    g_state = IDLE;
 }
 
 void data_sending_state(void)
